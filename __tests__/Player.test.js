c1449efe9205ef45fdb8a3b657c16e89b127fbae
shawn <desaunders10@gmail.com>
2021-01-16 15:24:40 -0600

    Added test for Player object

---
+++ __tests__/Player.test.js
@@ -0,0 +1,16 @@
+const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+test('creates a Player object', () => {
+  const player = new Player('Dave');
+
+  console.log(new Potion());
+  expect(player.name).toBe('Dave');
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
+  expect(player.inventory).toEqual(
+    expect.arrayContaining([expect.any(Object)])
+  );
+})
